fc18db816423de6b824ded2d735db9590033a7ed
oriolfornes <oriol.fornes@gmail.com>
2020-12-15 10:41:49 -0800

    added data

---
+++ data/Genomes/hg38/get_hg38.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ ! -f hg38.fa ]; then
+    wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/hg38.chromFa.tar.gz
+    tar xvfz hg38.chromFa.tar.gz 
+    find chroms -type f | grep -v "_" | perl -e 'while(<>){chomp;system("mv $_ .");}'
+    rm -r chroms/ hg38.chromFa.tar.gz
+    cat *.fa > hg38.fa
+fi
